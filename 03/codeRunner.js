8ed0a6f23fb5e721816e1b959aa58d1b69fec981
themion7 <themion@naver.com>
2022-08-21 00:33:26 +0900

    1장~3장

---
+++ 03/codeRunner.js
@@ -0,0 +1,7 @@
+// Ctrl + Alt + N
+
+const arr = [1, 2, 3]
+
+arr.forEach(console.log)
+// Node.js에 없는 alert 함수는 실행되지 않음
+arr.forEach(alert)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # javascript_practice
-모던 자바스크립트 Deep Dive
+모던 자바스크립트 Deep Dive 책을 공부하면서 예제 혹은 예제와 연관된 코드를 적는 공간
