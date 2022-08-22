ea023a8b132f8125444e1b322fc74dd3764f9029
themion7 <themion@naver.com>
2022-08-22 17:55:26 +0900

    5장, 6장

---
+++ 06/numericType.js
@@ -0,0 +1,16 @@
+let binary = 0b1000001
+let octal = 0o101
+let hex = 0x41
+let nan = NaN
+let nanString = 'NaN'
+
+console.log(binary)
+console.log(octal)
+console.log(hex)
+console.log(nan)
+console.log(nanString)
+
+console.log()
+console.log(10 / 0)
+console.log(10 / -0)
+console.log(1 * 'Number')
---
+++ 06/symbol.js
@@ -0,0 +1,11 @@
+let key1 = Symbol('key')
+let key2 = Symbol('key')
+
+console.log(key1)
+console.log(key2)
+
+console.log(key1 == key2)
+console.log(key1 === key2)
+
+console.log(key1.toString())
+console.log(key1.toString() === key2.toString())
